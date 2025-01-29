6867af72bbc919fe5029b0162eabebd0572de63d
dinodinker <dinodinker@gmail.com>
2025-01-29 01:34:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', async function() {
                     if (imageUrl.includes("drive.google.com")) {
                         const fileIdMatch = imageUrl.match(/[-\w]{25,}/);
                         if (fileIdMatch) {
-                            imageUrl = `https://lh3.googleusercontent.com/d/${fileIdMatch[0]}=s220`;
+                            imageUrl = `https://lh3.googleusercontent.com/d/${fileIdMatch[0]}`;
                         }
                     }
                     // Ensure the image icon is visible and full image opens on click
@@ -76,8 +76,8 @@ function showFullImage(imageUrl) {
     img.src = imageUrl;
     img.style.width = 'auto';
     img.style.height = 'auto';
-    img.style.maxWidth = '95%';
-    img.style.maxHeight = '95%';
+    img.style.maxWidth = '100%';
+    img.style.maxHeight = '100%';
     img.style.borderRadius = '10px';
     img.style.boxShadow = '0px 4px 10px rgba(0, 0, 0, 0.2)';
     
